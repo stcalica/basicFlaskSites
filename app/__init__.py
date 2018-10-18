321d09aa8bf2b990fc44e38acad79849e4a43269
kyle.calica-steinhilber <kyle.calica-steinhilber@workday.com>
2018-10-17 20:58:49 -0700

    defraktion simple design
    Change-Id: I7a71bce9cccbd7f146e6cab6bb8dce580da5e883

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,25 @@
 from flask import Flask
 import json
 import os
-files = os.listdir(".")
-print files
 
 app = Flask(__name__)
 
 with open("app/company.config.json", "r") as f:
 	    config = json.load(f)
 
+with open("app/events.config.json", "r") as f:
+	    events = json.load(f)
+
+with open("app/team.config.json", "r") as f:
+	    team = json.load(f)
+
 app.config.company = config["company"]
 app.config.brand_image_url = config["brand_image_url"]
+app.config.events = events
+app.config.team = team
+
+print app.config.company
+print app.config.team
+print app.config.events
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -6,3 +6,11 @@ from flask import render_template
 @app.route('/')
 def index():
 	return render_template("index.html", company=app.config.company, brand_image_url=app.config.brand_image_url)
+
+@app.route('/team')
+def team():
+	return render_template("team.html", company=app.config.company, brand_image_url=app.config.brand_image_url, team=app.config.team)
+
+@app.route('/events')
+def events():
+	return render_template("events.html", company=app.config.company, brand_image_url=app.config.brand_image_url, events=app.config.events)
