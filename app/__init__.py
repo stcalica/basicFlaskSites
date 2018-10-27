51a4a9d3b3fb0722168b94e973cc0dfedb562899
kyle calica <kycalica@gmail.com>
2018-10-27 01:32:20 +0000

    some changes to docker-image and routes for sitemap xml and to make docker ready

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ import os
 files = os.listdir(".")
 print files
 
-app = Flask(__name__, static_folder='static')
+app = Flask(__name__, static_folder='static', static_url_path='')
 
 with open("app/company.config.json", "r") as f:
 	    config = json.load(f)
